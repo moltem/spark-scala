a2901dfdad54245f631bf01059cafc02c45eb828
moltem <moltem@yandex.ru>
2022-04-18 20:48:28 +0300

    final

---
+++ kasp/spark/Q2/README.md
@@ -0,0 +1,16 @@
+2. Сколько партиций по умолчанию имеет DataFrame при чтении из JDBC источника? 
+Ответ: 1
+
+Partitioning with JDBC sources (https://medium.com/@radek.strnad/tips-for-using-jdbc-in-apache-spark-sql-396ea7b2e3d3)
+
+Traditional SQL databases can not process a huge amount of data on different nodes as a spark. In order to allow a
+Spark to read data from a database via JDBC in parallel, you must specify the level of parallel reads/writes which is
+controlled by the following option .option('numPartitions', parallelismLevel). The specified number controls a maximal
+number of concurrent JDBC connections. By default, you read data to a single partition which usually doesn't fully
+utilize your SQL database and obviously Spark.
+
+something like
+SELECT * FROM pets WHERE owner_id >= 1 and owner_id < 1000
+SELECT * FROM pets WHERE owner_id >= 1000 and owner_id < 2000
+SELECT * FROM pets WHERE owner_id >= 2000 and owner_id < 3000
+
---
+++ kasp/spark/Q2/src/main/scala/Q2.scala
@@ -0,0 +1,80 @@
+import org.apache.spark
+import org.apache.spark.sql.functions.{col, lit, upper}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.log4j.{Level, Logger}
+
+object Q2 extends App {
+
+  def printPhysicalPlan[_](ds: Dataset[_]): Unit = {
+    println(ds.queryExecution.executedPlan.treeString)
+
+  }
+
+  val rootLogger = Logger.getRootLogger()
+  rootLogger.setLevel(Level.ERROR)
+  Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+  Logger.getLogger("org.spark-project").setLevel(Level.ERROR)
+
+    //===========================//
+    //creating SparkSession
+    //===========================//
+    val spark_posgreSQlReader = SparkSession.builder()
+      .appName("Postgresql_Connector_Spark")
+      .config("spark.master", "local[*]")
+      .getOrCreate()
+
+    val jdbcUrl = "jdbc:postgresql://localhost:5432/posgreplayground?user=docker&password=docker"
+
+    val dfSalaries = spark_posgreSQlReader
+      .read
+      .format("jdbc")
+      .option("url", jdbcUrl)
+      .option("driver", "org.postgresql.Driver")
+      .option("dbtable", "salaries")
+      .load()
+
+    println("++++++++printPhysicalPlan: numPartitions=1 +++++++++++++++++++++++++++++++++++++++++++++++")
+    println(dfSalaries.queryExecution.toString())
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+    println("++++++++printPhysicalPlan: numPartitions=1 +++++++++++++++++++++++++++++++++++++++++++++++")
+    printPhysicalPlan(dfSalaries.select())
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+    dfSalaries.show()
+
+
+    val dfSalariesParallel = spark_posgreSQlReader
+      .read
+      .format("jdbc")
+      .option("url", jdbcUrl)
+      .option("driver", "org.postgresql.Driver")
+      .option("dbtable", "salaries")
+      .option("numPartitions", 10)
+      .option("partitionColumn", "emp_no")
+      .option("lowerBound", 10010)
+      .option("upperBound", 499990)
+      .load()
+
+    /* something like
+    SELECT * FROM pets WHERE owner_id >= 1 and owner_id < 1000
+    SELECT * FROM pets WHERE owner_id >= 1000 and owner_id < 2000
+    SELECT * FROM pets WHERE owner_id >= 2000 and owner_id < 3000
+    */
+
+
+    println("++++++++printPhysicalPlan: numPartitions=10 ++++++++++++++++++++++++++++")
+    println(dfSalariesParallel.queryExecution.toString())
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+    println("++++++++printPhysicalPlan: numPartitions=10 ++++++++++++++++++++++++++++")
+    printPhysicalPlan(dfSalariesParallel.select())
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+    dfSalariesParallel.show()
+
+
+
+}
